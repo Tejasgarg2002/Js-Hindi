170600db63b5149b267be67ff359e4d1c9c40aef
Garg_Tejas <97401487+Tejasgarg2002@users.noreply.github.com>
2024-08-28 12:10:23 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+console.log(3
+    +3 // code readability should be high
+)
+
+console.log("Tejas")
+
+let name="tejas"
+let age=21
+let isLoggedIn=true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+//null => standalone value
+// undefined =>
+//symbol => unique
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object

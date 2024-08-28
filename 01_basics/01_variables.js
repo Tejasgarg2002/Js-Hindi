ebd11e6b0bb7271a054b7ef80acfa124ae5675ac
Garg_Tejas <97401487+Tejasgarg2002@users.noreply.github.com>
2024-08-28 11:25:09 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const account_id=14453;
+let accountEmail="tejas@gmail.com";
+var accountPassword="12345";
+accountCity="Delhi";
+let accountState;
+
+// accountId=2 //Not allowed
+
+accountEmail="tej@gmail.com";
+accountPassword="3455";
+accountCity="Hyderabad";
+
+
+/*prefer not use var
+because of issue in block scope and functional scope
+*/
+console.table([account_id,accountEmail,accountPassword,accountCity,accountState]);

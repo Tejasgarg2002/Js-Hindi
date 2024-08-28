7c2b5d1e7bdbd174f53e15c2c9d3ab2339e7ecba
Garg_Tejas <97401487+Tejasgarg2002@users.noreply.github.com>
2024-08-28 12:25:11 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="Tejas"
+
+// console.log(score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true=>1; false=>0
+
+let isLoggedIn="tejas"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1=>true ; 0=>false
+// "" => false
+// "tejas" => true
+
+let someNumber=33
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber)

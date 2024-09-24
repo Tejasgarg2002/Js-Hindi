b02d3a63582528bd9be85d44822e26501ce56e2b
Garg_Tejas <97401487+Tejasgarg2002@users.noreply.github.com>
2024-09-24 05:49:41 +0000

    dataype_summary

---
+++ 01_basics/05_dataTypes_Summary.js
@@ -0,0 +1,32 @@
+// Primitive
+// 7 Types : string,Number,Boolean,null,undefined,symbol,Bigint
+
+const score=100
+const scoreValue=100.3
+const isLoggedIn = true
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol("123")
+const anotherId=Symbol("123")
+
+console.log(id===anotherId)
+
+const bigNumber=39284792374932749n
+
+// Refrence (Non Primitive)
+
+// Array,Objects,Functions
+
+const Heros = ["Shaktiman","Nagraj","Doga"]
+let myObj={
+    name:"Tejas",
+    age:21
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof bigNumber)
+
